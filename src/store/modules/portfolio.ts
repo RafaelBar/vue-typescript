e56172a5f487a17781fe3116724fc25fa9ba7fac
Rafael Bar <rafael@wayoutgrouplabs.com>
2020-04-09 16:15:06 +0300

    Auto stash before rebase of "refs/heads/master"

--- src/store/modules/portfolio.ts
+++ src/store/modules/portfolio.ts
@@ -2,6 +2,7 @@ import { VuexModule, Module, Mutation, Action } from 'vuex-module-decorators'
 
 @Module
 class Portfolio extends VuexModule {
+<<<<<<< Updated upstream
   public funds: any = 10000
   public stocks: any[] = []
 
@@ -9,6 +10,15 @@ class Portfolio extends VuexModule {
   public 'BUY_STOCK' (order: any) {
     debugger
       const record = this.stocks.find(element => element.id == order.stockId);
+=======
+  public funds: number = 1000
+  public stocks: any[] = []
+  
+
+  @Mutation
+  public 'BUY_STOCK' (order: StockObj) {
+      const record = this.stocks.find(element => element.id == order.id);
+>>>>>>> Stashed changes
       if(record){
         order.quantity = parseInt(order.quantity);
           record.quantity = parseInt(record.quantity);
@@ -16,17 +26,29 @@ class Portfolio extends VuexModule {
       }
       else{
           this.stocks.push({
+<<<<<<< Updated upstream
               id: order.stockId,
+=======
+              id: order.id,
+>>>>>>> Stashed changes
               quantity: order.quantity,
               price: order.stockPrice,
               name: order.name
           })
       }
+<<<<<<< Updated upstream
       this.funds -= order.stockPrice * order.quantity;
   }
   
   //Getters
   get stockPortfolio(): any {
+=======
+      this.funds -= order.price * order.quantity;
+  }
+  
+  //Getters
+  get stockPortfolio(): StockObj[] {
+>>>>>>> Stashed changes
           return this.stocks.map(stock => {
               const record = this.stocks.find(element => element.id == stock.id);
               return{
@@ -37,7 +59,11 @@ class Portfolio extends VuexModule {
               }
           });
   }
+<<<<<<< Updated upstream
   get _funds(): any {
+=======
+  get _funds(): number {
+>>>>>>> Stashed changes
     return this.funds
   }
 }
